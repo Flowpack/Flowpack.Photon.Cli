a45a1f8d96fbbf6c1e21a2190096e2fd0f5e77fa
Christopher Hlubek <hlubek@networkteam.com>
2018-03-12 18:02:08 +0100

    Task: Support content from specific paths

--- Classes/Command/GenerateCommandController.php
+++ Classes/Command/GenerateCommandController.php
@@ -19,15 +19,17 @@ class GenerateCommandController extends CommandController
     /**
      * Generate content
      *
-     * Generate output of a static publishing target with content in a content package
+     * Generate output of a static publishing target based on a package declaring the rendering
+     * and content (or content from a specific directory)
      *
-     * @param string $packageKey Package key of a content package
+     * @param string $packageKey Package key of a package declaring the rendering and content (can be overridden by contentPath)
      * @param string $targetName Name of the publishing target to generate
+     * @param string $contentPath Optional path to content if not using a package
      * @return void
      */
-    public function contentCommand(string $packageKey, string $targetName = 'default'): void
+    public function contentCommand(string $packageKey, string $targetName = 'default', string $contentPath = null): void
     {
-        $results = $this->generator->generate($packageKey, $targetName);
+        $results = $this->generator->generate($packageKey, $targetName, $contentPath);
         foreach ($results as $result) {
             $this->outputLine((string)$result);
         }
