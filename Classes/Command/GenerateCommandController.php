4d3252dc538b106b8cd23102c074ab6e744fbefa
Christopher Hlubek <hlubek@networkteam.com>
2018-03-12 17:22:14 +0100

    Task: Vendor renamed to Flowpack

---
+++ Classes/Command/GenerateCommandController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Neos\Photon\Cli\Command;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Cli\CommandController;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class GenerateCommandController extends CommandController
+{
+
+    /**
+     * @Flow\Inject
+     * @var \Neos\Photon\Common\Generator\GeneratorInterface
+     */
+    protected $generator;
+
+    /**
+     * Generate content
+     *
+     * Generate output of a static publishing target with content in a content package
+     *
+     * @param string $packageKey Package key of a content package
+     * @param string $targetName Name of the publishing target to generate
+     * @return void
+     */
+    public function contentCommand(string $packageKey, string $targetName = 'default'): void
+    {
+        $results = $this->generator->generate($packageKey, $targetName);
+        foreach ($results as $result) {
+            $this->outputLine((string)$result);
+        }
+    }
+}
